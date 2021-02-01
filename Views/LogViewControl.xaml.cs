42e4849b91e3490a379e8addc3ccfc335545a7e0
aa <a@h>
2021-02-01 10:56:22 -0800

    Add project files.

---
+++ Views/LogViewControl.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading;
+using System.Windows.Controls;
+using LogViewExample.Models;
+
+namespace LogViewExample.Views
+{
+    /// <summary>
+    /// Interaction logic for LogViewControl.xaml
+    /// </summary>
+    public partial class LogViewControl : UserControl
+    {
+        ViewModels.LogView logView;
+        ObservableCollection<LogEntry> logEntries;
+        Timer timer;
+        public LogViewControl()
+        {
+            InitializeComponent();
+
+            logView = new ViewModels.LogView();
+            logEntries = logView.LogEntries;
+            timer = new Timer(x => AddRandomEntry(), null, 1000, 10);
+
+            DataContext = logEntries;
+        }
+
+        private void AddRandomEntry()
+        {
+            Dispatcher.BeginInvoke((Action)(() => logEntries.Add(logView.GetRandomEntry())));
+        }
+    }
+}
